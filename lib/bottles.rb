3721ca0eff5a05a19dac47fcf37cf1c0d07fa23b
Justin Unton <junton@gmail.com>
2019-03-11 21:22:21 -0400

    added model and method

---
+++ lib/bottles.rb
@@ -0,0 +1,8 @@
+class Bottles
+	def verse(bottles)
+		"#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
+"
+	end
+end
+
