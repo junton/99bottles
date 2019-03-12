837ee34fbfe3dbc9eaa1731d89b6ba09ead8ae35
Justin Unton <junton@gmail.com>
2019-03-11 22:19:20 -0400

    int to string so I can downcase

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,17 +6,19 @@ class Bottles
 	def first_section(bottles)
 		ending = first_suffix(bottles)
 
-		number_of_bottles = bottles
+		number_of_bottles = bottles.to_s
 		if(bottles == 0)
 			number_of_bottles = "No more"
 		end
 
-		"#{number_of_bottles} bottle#{ending} of beer on the wall, #{number_of_bottles} bottle#{ending} of beer."
+		"#{number_of_bottles} bottle#{ending} of beer on the wall, #{number_of_bottles.downcase} bottle#{ending} of beer."
 	end
 
 	def second_section(bottles)
 		ending = second_suffix(bottles)
-		if bottles == 1
+		if bottles == 0
+			"Go to the store and buy some more, #{bottles + 99} bottles of beer on the wall."
+		elsif bottles == 1
 			"Take it down and pass it around, no more bottles of beer on the wall."
 		else
 			"Take one down and pass it around, #{bottles-1} bottle#{ending} of beer on the wall."
